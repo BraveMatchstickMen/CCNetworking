4dce8989de3a4159ec579ce19629a4622fc42bda
chai <bravetsia@gmail.com>
2019-04-19 14:55:57 +0800

    Init Project

---
+++ CCNetworking/CCNetworking.h
@@ -0,0 +1,19 @@
+//
+//  CCNetworking.h
+//  CCNetworking
+//
+//  Created by chai.chai on 2019/4/19.
+//  Copyright © 2019 chai.chai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CCNetworking.
+FOUNDATION_EXPORT double CCNetworkingVersionNumber;
+
+//! Project version string for CCNetworking.
+FOUNDATION_EXPORT const unsigned char CCNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCNetworking/PublicHeader.h>
+
+

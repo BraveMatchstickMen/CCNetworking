480d3eaa899d7f73d4c5362d3425ac94a78d7c9f
chai <bravetsia@gmail.com>
2019-06-04 19:15:28 +0800

    separate request encoder

--- CCNetworking/Request.swift
+++ CCNetworking/Request.swift
@@ -27,7 +27,7 @@ public class Request {
 
         var urlPath = url
         if method == .get {
-            urlPath += "?" + Request.buildParams(parameters)
+            urlPath += "?" + RequestEncoder.buildParams(parameters)
         }
 
         guard let url = URL(string: urlPath.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!) else {
@@ -39,7 +39,7 @@ public class Request {
 
         if method == .post {
             request.addValue(contentType.rawValue, forHTTPHeaderField: "Content-Type")
-            request.httpBody = Request.buildParams(parameters).data(using: .utf8)
+            request.httpBody = RequestEncoder.buildParams(parameters).data(using: .utf8)
         }
 
         let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
@@ -47,44 +47,5 @@ public class Request {
         }
 
         task.resume()
-    }
-}
-
-private extension Request {
-    static func buildParams(_ parameters: [String: AnyObject]) -> String {
-        var components: [(String, String)] = []
-        for key in Array(parameters.keys).sorted(by: <) {
-            let value: AnyObject! = parameters[key]
-            components += queryComponents(key, value)
-        }
-
-        return components.map { "\($0) = \($1)" }.joined(separator: "&")
-    }
-
-    static func queryComponents(_ key: String, _ value: AnyObject) -> [(String, String)] {
-        var components: [(String, String)] = []
-        if let dictionary = value as? [String: AnyObject] {
-            for (nestedKey, value) in dictionary {
-                components += queryComponents("\(key)[\(nestedKey)]", value)
-            }
-        } else if let array = value as? [AnyObject] {
-            for value in array {
-                components += queryComponents("\(key)", value)
-            }
-        } else {
-            components.append((escape(key), escape("\(value)")))
-        }
-
-        return components
-    }
-
-    static func escape(_ string: String) -> String {
-        let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
-        let subDelimitersToEncode = "!$&'()*+,;="
-
-        var allowedCharacterSet = CharacterSet.urlQueryAllowed
-        allowedCharacterSet.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
-
-        return string.addingPercentEncoding(withAllowedCharacters: allowedCharacterSet) ?? string
     }
 }
---
+++ CCNetworking/RequestEncoder.swift
@@ -0,0 +1,50 @@
+//
+//  RequestEncoder.swift
+//  CCNetworking
+//
+//  Created by chai.chai on 2019/6/4.
+//  Copyright © 2019 chai.chai. All rights reserved.
+//
+
+import Foundation
+
+struct RequestEncoder {
+    static func buildParams(_ parameters: [String: AnyObject]) -> String {
+        var components: [(String, String)] = []
+        for key in Array(parameters.keys).sorted(by: <) {
+            let value: AnyObject! = parameters[key]
+            components += queryComponents(key, value)
+        }
+
+        return components.map { "\($0) = \($1)" }.joined(separator: "&")
+    }
+}
+
+private extension RequestEncoder {
+    static func queryComponents(_ key: String, _ value: AnyObject) -> [(String, String)] {
+        var components: [(String, String)] = []
+        if let dictionary = value as? [String: AnyObject] {
+            for (nestedKey, value) in dictionary {
+                components += queryComponents("\(key)[\(nestedKey)]", value)
+            }
+        } else if let array = value as? [AnyObject] {
+            for value in array {
+                components += queryComponents("\(key)", value)
+            }
+        } else {
+            components.append((escape(key), escape("\(value)")))
+        }
+
+        return components
+    }
+
+    static func escape(_ string: String) -> String {
+        let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
+        let subDelimitersToEncode = "!$&'()*+,;="
+
+        var allowedCharacterSet = CharacterSet.urlQueryAllowed
+        allowedCharacterSet.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
+
+        return string.addingPercentEncoding(withAllowedCharacters: allowedCharacterSet) ?? string
+    }
+}
